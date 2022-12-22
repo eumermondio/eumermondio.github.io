2181c5df8ac44489ca291141792784a9ab62e4ca
eumermondio <cpachecoarjona@gmail.com>
2022-12-22 18:08:07 +0100

    Añadida primera imagen y layout completo de la pagina

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 //
 // Poner aquí los scripts 
 // 
+document.addEventListener("DOMContentLoaded", e => {
+    new fullpage('#main', {
+        licenseKey: "gplv3-license",
+        autoScrolling: true,
+        scrollHorizontally: false,
+        sectionsColor: ['black', 'white', 'black', 'white'],
+        loopBottom: true,
+        navigation: true,
+        slidesNavigation: true,
+        controlArrows: false,
+    });
+    let botonesAbajo = document.querySelector("ul");
+    botonesAbajo.style.textAlign = "center";
+});
